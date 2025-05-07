6a5956cb4a3aa6ff831e29f9a366f201c060ee4c
Nothing <155074292+MRNobodyFA@users.noreply.github.com>
2025-05-07 08:35:06 +0330

    Update MainActivity.java

--- app/src/main/java/com/example/androidengine/MainActivity.java
+++ app/src/main/java/com/example/androidengine/MainActivity.java
@@ -1,1 +1,9 @@
+package com.example.androidengine;
 
+import android.app.NativeActivity;
+
+// در این نمونه، از NativeActivity استفاده می‌شود تا کتابخانه نیتیو (libandroidengine.so)
+// به صورت خودکار در زمان اجرا بارگذاری شود.
+public class MainActivity extends NativeActivity {
+    // این کلاس برای افزودن قابلیت‌های جاوایی یا تعامل‌های بیشتر قابل توسعه است.
+}
